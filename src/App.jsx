45e0b90d07636280a934acf5889539b1ca39ae12
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-07-13 17:52:32 +0530

    completed cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { ScrollTrigger,SplitText } from 'gsap/all'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import Cocktails from './components/Cocktails'
 gsap.registerPlugin(ScrollTrigger, SplitText)
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <>
       <NavBar/>
       <Hero/>
+      <Cocktails/>
     </>
   )
 }

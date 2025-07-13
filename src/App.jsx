23099a1a6d5037cbbd67dc16496756a2605d00c6
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-07-13 17:00:27 +0530

    Hero section scroll animation completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import { useState } from 'react'
 import gsap from 'gsap'
 import './App.css'
 import { ScrollTrigger,SplitText } from 'gsap/all'
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 gsap.registerPlugin(ScrollTrigger, SplitText)
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-       <h1 className="text-3xl text-indigo-300">
-        MOJITO.....
-       </h1>
+      <NavBar/>
+      <Hero/>
     </>
   )
 }
